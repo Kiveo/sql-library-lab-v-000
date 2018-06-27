5a4ddb77aba8cdac869baa70d3d0bb73b981e07e
Maverick Tesini <mavericktesini@gmail.com>
2018-06-27 05:36:55 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,8 +9,12 @@
 --
 -- Feel free to make these up if you don't know any Fantasy series :)
 
-INSERT INTO series (title) VALUES ("Harry Potter"), ("Seven Swords");
+INSERT INTO series (title) VALUES ("Harry Potter"), ("Book of Swords");
 
 INSERT INTO books (title) VALUES ("Harry 1"), ("Harry 2"), ("Harry 3"), ("Sword 1"), ("Sword 2"), ("Sword 3");
 
 INSERT INTO characters (name) VALUES ("Harry"), ("Ron"), ("Hagrid"), ("Hermoine"), ("Fencer"), ("Knight"), ("Ninja"), ("Samurai"); 
+
+INSERT INTO subgenres (name) VALUES ("science fiction"), ("fantasy");
+
+INSERT INTO authors (name) VALUES ("J. K. Rowling"), ("Fred Saberhagen");

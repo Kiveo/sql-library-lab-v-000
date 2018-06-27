d877447a1aaadecfc91d2f3974bae20e6ae0ed87
Maverick Tesini <mavericktesini@gmail.com>
2018-06-27 05:21:54 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,8 +4,8 @@
 -- Authors have a name
 -- Books have a title and year and belong to a series
 -- Characters have a name, motto, and species and belong to an author and a series
--- Books have many characters and characters are in many books in a series. How do we accomplish this complex association? 
--- With a join table between Characters and Books. This join table 
+-- Books have many characters and characters are in many books in a series. How do we accomplish this complex association?
+-- With a join table between Characters and Books. This join table
 -- (let's call it character_books) will just have -in addition to its primary key- two foreign key columns for the character and book ids. Each row in this join table acts as a relation between a book and a character.
 
 CREATE TABLE series (
@@ -29,7 +29,7 @@ CREATE TABLE books (
   id INTEGER PRIMARY KEY,
   title TEXT,
   year INTEGER,
-  series_id INTEGER,
+  series_id INTEGER
 );
 
 CREATE TABLE characters (
@@ -38,7 +38,7 @@ CREATE TABLE characters (
   species TEXT,
   motto TEXT,
   series_id INT,
-  author_id INT,
+  author_id INT
 );
 
 CREATE TABLE character_books (

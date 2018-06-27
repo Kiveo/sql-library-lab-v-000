9447944d68619b80f9edea4dd5e51657ab0b4e7a
Maverick Tesini <mavericktesini@gmail.com>
2018-06-27 05:28:34 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+-- 2 series
+-- 2 sub-genres
+-- 2 authors
+-- 3 books in each series
+-- 8 characters
+-- 4 characters in each series
+-- of each of those 4, make 2 in all of the books in a series, and 2 in just 1 book in a series
+-- Note you will need to insert values into your character_books join table
+--
+-- Feel free to make these up if you don't know any Fantasy series :)
+
+INSERT INTO series (title) VALUES ("Harry Potter"), ("Seven Swords");

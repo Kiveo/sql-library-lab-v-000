ae9d2dee483b28a53e186a0ba8059674d18778e3
Maverick Tesini <mavericktesini@gmail.com>
2018-06-27 05:52:56 +0000

    automatically backed up by learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,3 +1,3 @@
 -- Update the species of the last character in the database to "Martian" by writing an update statement in update.sql
 
-UPDATE characters (species) WHERE id = 8 SET "Martian";
+UPDATE characters (species) WHERE id = 8 SET species = "Martian";
